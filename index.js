6c6eb4643c3964c256e1d5ed72023e8570521c90
t dis <thomdisley@gmail.com>
2017-09-14 17:30:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
-// function shout(string) {
-//   return string.toUpperCase()
-// }
-//
-// function whisper(string) {
-//   return string.toLowerCase()
-// }
-//
-// function logShout(string) {
-//   console.log(string.toUpperCase());
-// }
-//
-// function logWhisper(string) {
-//   console.log(string.toLowerCase());
-// }
+function shout(string) {
+  return string.toUpperCase()
+}
 
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
     return x = "I can't hear you!"
   }
 }
- 
-sayHiToGrandma('hey');
 
-console.log(x);
